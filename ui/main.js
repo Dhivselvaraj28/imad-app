23b0f72edfed0bfce8227448649a56e2adaab68f
Dhivselvaraj28 <31008276+Dhivselvaraj28@users.noreply.github.com>
2017-08-20 18:39:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,14 @@
 console.log('Loaded!');
-var element=document.getElementById('main text');
+var element = document.getElementById('main text');
 element.innerHTML="WEB APP"
 
 var img = document.getElementById('madi');
 img = onclick.function (){
     img.style.marginLeft='100px';
+}
+var button = document.getElementById('counter');
+button.onclick = function() {
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span innerHTML = counter.toString();
 }
